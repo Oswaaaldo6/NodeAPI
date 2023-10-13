a2c01957c96a16743e135951aad294b8fe44743b
$ickOs$ <juand_david2001@hotmail.com>
2023-10-13 00:34:23 -0600

    JSON proxy size created #1

---
+++ index.js
@@ -0,0 +1,23 @@
+const cors = require('cors')
+const express = require('express')
+
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+
+const port = 8080;
+
+app.get('/', (req, res, next) => {
+	res.send('node express api <br> by CrazyDave');
+});
+
+app.get('/proxySize', (req, res, next)=>{
+	res.sendfile("assets/a1_proxy_size.json")
+})
+
+app.listen(port,  () => 
+	console.log('listening on port ' + port
+));
+
+
