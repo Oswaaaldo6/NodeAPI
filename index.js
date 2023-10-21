3fa40e0ace05ecf1ebc752ef358a5c750c623828
$ickOs$ <juand_david2001@hotmail.com>
2023-10-21 15:07:06 -0600

    json file 3a_test3 #5

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/3aTest2', (req, res, next)=>{
 	res.sendfile("assets/3a_test2.json")
 })
 
+app.get('/3aTest3', (req, res, next)=>{
+	res.sendfile("assets/3a_test3.json")
+})
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
